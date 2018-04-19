bb6ea1ac49fbba2b87bc9d6e54bb0b1a7722c0e8
Oleksii Kolyvayko <kolyvayko@gmail.com>
2018-04-19 13:44:58 +0300

    add hello api methods

--- backend/controllers/helloapi.go
+++ backend/controllers/helloapi.go
@@ -24,9 +24,12 @@ func (c *HelloAPIController) responseWithError(status int, message map[string]st
 }
 
 type Hello struct {
-	Address string
-	PubKey  string
-	AES     string
+	PrivKey   string
+	Address   string
+	PubKey    string
+	AES       string
+	Url       string
+	ServerKey string
 }
 
 // Post ...
@@ -45,37 +48,62 @@ func (c *HelloAPIController) Post() {
 		c.responseWithError(400, map[string]string{"message": err.Error()}, err)
 		return
 	}
-	id := c.Ctx.Input.Param(":id")
+	if ob.PubKey == "" || ob.AES == "" || ob.Address == "" {
+		c.responseWithError(400, map[string]string{"message": "empty field(s)"}, "empty field(s)")
+		return
+	}
 
 	o := orm.NewOrm()
 	o.Using("default")
+	// check user application acc
+	u := models.Users{}
+	err = o.QueryTable("users").Filter("public_key", ob.PubKey).Limit(1).One(&u)
+	beego.Warn(err)
+	if err == orm.ErrNoRows && ob.PrivKey != "" { // if not exist
+		// create new acc ///////////////
+		csk, err := services.GetAESEncode(ob.PrivKey, services.GetMD5Hash(ob.AES))
 
-	switch id {
-	case "application":
-		// check user application acc
-		u := models.Users{}
-		err := o.QueryTable("users").Filter("public_key", ob.PubKey).Limit(1).One(&u)
+		if err != nil {
+			c.responseWithError(500, map[string]string{"message": err.Error()}, err)
+			return
+		}
 
-		if err == orm.ErrNoRows { // if not exist
-			// create new acc ///////////////
-			u.PublicKey = ob.PubKey
-			u.Address = ob.Address
-			u.Password = services.GetHash(ob.AES)
-			u.Role = 20
-			u.Status = false
+		u.PublicKey = ob.PubKey
+		u.PrivateKey = csk
+		u.Address = ob.Address
+		u.Password = services.GetHash(ob.AES)
+		u.Role = 20
+		u.Status = false
 
-			_, err = o.Insert(&u)
-			if err != nil { // if  insert error
-				c.responseWithError(400, map[string]string{"message": err.Error()}, err)
-				return
-			}
-			/////////////////////////////////
-
-		} else if err != nil { // if others error
+		_, err = o.Insert(&u)
+		if err != nil { // if  insert error
 			c.responseWithError(400, map[string]string{"message": err.Error()}, err)
 			return
 		}
-		if u.Password == services.GetHash(ob.AES) {
+		/////////////////////////////////
+	} else if err == orm.ErrNoRows && ob.PrivKey == "" {
+		c.responseWithError(400, map[string]string{"message": "PrivKey is empty"}, "PrivKey is empty")
+		return
+	} else if err != nil { // if others error
+		c.responseWithError(400, map[string]string{"message": err.Error()}, err)
+		return
+
+	}
+
+	_, err = services.GetAESDecode(u.PrivateKey, services.GetMD5Hash(ob.AES))
+
+	if err != nil {
+		beego.Error(err)
+		c.responseWithError(400, map[string]string{"message": err.Error()}, err)
+		return
+	} else {
+		//if u.Password == services.GetHash(ob.AES) {
+		id := c.Ctx.Input.Param(":id")
+		/////////////////////////
+		// switch type of servers
+		/////////////////////////
+		switch id {
+		case "application":
 			bcNodes, _, _, suspicios, err := getTotals()
 			if err != nil { // if others error
 				c.responseWithError(400, map[string]string{"message": err.Error()}, err)
@@ -104,32 +132,72 @@ func (c *HelloAPIController) Post() {
 				"bcNodes":      blockchain,
 				"raftNodes":    raft,
 				"storageNodes": storage,
-				"spaceleft":    0,
+				"spaceleft":    100,
 			}
-		} else {
-			c.responseWithError(400, map[string]string{"message": "not permited"}, "not permited")
+
+		case "blockchain":
+			if ob.Url != "" {
+				exist, err := addServer(id, ob, u)
+				if exist {
+					c.responseWithError(200, map[string]string{"message": "Welcome back"}, "Welcome back")
+					return
+				} else if err != nil { // if others error
+					c.responseWithError(400, map[string]string{"message": err.Error()}, err)
+					return
+
+				} else {
+					c.responseWithError(200, map[string]string{"message": "Hello"}, "Hello")
+					return
+				}
+			} else {
+				c.responseWithError(400, map[string]string{"message": "url not set"}, "url not set")
+				return
+			}
+		case "raft":
+			if ob.Url != "" {
+				exist, err := addServer(id, ob, u)
+				if exist {
+					c.responseWithError(200, map[string]string{"message": "Welcome back"}, "Welcome back")
+					return
+				} else if err != nil { // if others error
+					c.responseWithError(400, map[string]string{"message": err.Error()}, err)
+					return
+
+				} else {
+					c.responseWithError(200, map[string]string{"message": "Hello"}, "Hello")
+					return
+				}
+			} else {
+				c.responseWithError(400, map[string]string{"message": "url not set"}, "url not set")
+				return
+			}
+		case "storage":
+			if ob.Url != "" {
+				exist, err := addServer(id, ob, u)
+				if exist {
+					c.responseWithError(200, map[string]string{"message": "Welcome back"}, "Welcome back")
+					return
+				} else if err != nil { // if others error
+					c.responseWithError(400, map[string]string{"message": err.Error()}, err)
+					return
+
+				} else {
+					c.responseWithError(200, map[string]string{"message": "Hello"}, "Hello")
+					return
+				}
+			} else {
+				c.responseWithError(400, map[string]string{"message": "url not set"}, "url not set")
+				return
+			}
+		default:
+			c.responseWithError(403, map[string]string{"message": "Permission denied"}, "Permission denied")
 			return
 		}
-
-	case "blockchain":
-		c.Data["json"] = map[string]interface{}{
-			"hello": ob,
-			"id":    id,
-		}
-	case "raft":
-		c.Data["json"] = map[string]interface{}{
-			"hello": ob,
-			"id":    id,
-		}
-	case "storage":
-		c.Data["json"] = map[string]interface{}{
-			"hello": ob,
-			"id":    id,
-		}
-	default:
-		c.responseWithError(403, map[string]string{"message": "Permission denied"}, "Permission denied")
-		return
 	}
+	//} else {
+	//	c.responseWithError(400, map[string]string{"message": "not permited"}, "not permited")
+	//	return
+	//}
 
 	c.ServeJSON()
 	c.StopRun()
@@ -153,12 +221,32 @@ func getServerList(t string) ([]string, error) {
 	o.Using("default")
 	var list []string
 	num, err := o.Raw("SELECT url FROM serverListView WHERE role=?", t).QueryRows(&list)
-	beego.Warn(list)
-	beego.Error(err)
 	if err == nil && num > 0 { // if others error
 		return list, err
 	} else {
 		return nil, err
 	}
 
+}
+
+func addServer(role string, hello Hello, u models.Users) (bool, error) {
+	o := orm.NewOrm()
+	o.Using("default")
+	s := models.Servers{}
+
+	err := o.QueryTable("servers").Filter("role", role).Filter("url", hello.Url).Limit(1).One(&s)
+	exist := false
+	if err == orm.ErrNoRows {
+		// create new server ///////////////
+		s.Role = role
+		s.User = &u
+		s.Url = hello.Url
+		s.Name = hello.ServerKey
+		_, err = o.Insert(&s)
+
+	} else {
+		exist = true
+	}
+
+	return exist, err
 }
